d4ac4ebdc695561fbb505acb24f30fca6ffdc12a
G808 <hucciallstars@gmail.com>
2023-02-28 01:35:24 +0200

    Unfinished

--- app/components/screens/homepage/Map.tsx
+++ app/components/screens/homepage/Map.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
 const MapComponent = () => {
-  const apiKey = 'AIzaSyAq_Ns6uh4UCvKUjcwt9b9kNG1fsADGK1Y';
   return (
     <div className='h-screen w-full'>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: String({ apiKey }) }}
+        bootstrapURLKeys={{ key: String(...addAPIKEY) }}
         defaultCenter={{ lat: 50.450001, lng: 30.523333 }}
         options={{
           fullscreenControl: false,
